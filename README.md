08ea7c44b24f62bf37295acb7119ced6f0fd4c84
70_10 <7010.dev@gmail.com>
2016-04-03 01:01:33 +0900

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# React Boilerplate
+
+## Usage
+
+### install
+
+```
+$ npm install
+```
+
+### start
+
+```
+$ npm start
+```
+
+### build
+
+```
+$ npm run build
+```
---
+++ src/components/Counter/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import style from "./style.css";
+
+export default class Countrer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={style.normal}>{this.props.count}</div>
+        <button onClick={() => this.props.dispatch("increment", 1)}>+1</button>
+        <button onClick={() => this.props.dispatch("decrement", 1)}>-1</button>
+        <button onClick={() => this.props.dispatch("increment", 100)}>+100</button>
+      </div>
+    );
+  }
+}
