220c8f299db19d76cad43b2d1eb374a05c24d32f
70_10 <7010.dev@gmail.com>
2017-08-26 09:58:13 +0900

    [mod] webpack3

--- src/app.js
+++ src/app.js
@@ -1,21 +1,4 @@
-import {render} from "react-dom";
+import { render } from "react-dom";
 import React from "react";
-import injectTapEventPlugin from "react-tap-event-plugin";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import TopContainer from "./containers/top";
 
-import "normalize.css";
-import "material-design-icons";
-
-injectTapEventPlugin();
-
-const App = () => (
-  <MuiThemeProvider>
-    <TopContainer />
-  </MuiThemeProvider>
-);
-
-render(
-  <App />,
-  document.getElementById("app")
-);
+render(<div>Hogehoge</div>, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,28 @@
-const webpack = require("webpack");
 const path = require("path");
-const rucksack = require("rucksack-css");
 
 module.exports = {
-  context: path.join(__dirname, "./src"),
   entry: {
-    js: "./app.js",
-    html: "./index.html",
-    vendor: ["react"],
+    bundle: "./src/app.js"
   },
   output: {
-    path: "dist",
-    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
   },
   module: {
     loaders: [
       {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]",
-      },
-      {
-        test: /\.css$/,
-        include: /src/,
-        loaders: [
-          "style-loader",
-          "css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
-          "postcss-loader"
-        ]
-      },
-      {
-        test: /\.css$/,
-        exclude: /src/,
-        loader: "style!css"
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: ["react-hot", "babel-loader"],
-      },
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
     ]
   },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  postcss: [
-    rucksack({
-      autoprefixer: true
-    })
-  ],
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": { NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development") }
-    }),
-    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-  ],
   devServer: {
-    contentBase: "./src",
-    hot: true,
-    quiet: true
+    compress: true,
+    port: 8080
   }
 };
